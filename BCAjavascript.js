571c447ca9ab11d88f6233a8f70749a918249ff3
navdeep94374 <139533771+navdeep94374@users.noreply.github.com>
2023-07-14 12:06:53 +0530

    Add files via upload
    initial commit

---
+++ BCAjavascript.js
@@ -0,0 +1,86 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.navlist');
+
+burger.addEventListener('click', () => {
+    navlist.classList.toggle('vclass');
+    navbar.classList.toggle('hnav');
+})
+
+function sem1()
+    {
+    let showhidesem1=document.getElementById('showhidesem1');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem1.style.display!='none')
+    {
+        showhidesem1.style.display='none';
+    }
+    else{
+        showhidesem1.style.display='block';
+    }
+    }
+
+    function sem2()
+    {
+    let showhidesem2=document.getElementById('showhidesem2');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem2.style.display!='none')
+    {
+        showhidesem2.style.display='none';
+    }
+    else{
+        showhidesem2.style.display='block';
+    }
+    }
+
+    function sem3()
+    {
+    let showhidesem3=document.getElementById('showhidesem3');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem3.style.display!='none')
+    {
+        showhidesem3.style.display='none';
+    }
+    else{
+        showhidesem3.style.display='block';
+    }
+    }
+
+    function sem4()
+    {
+    let showhidesem4=document.getElementById('showhidesem4');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem4.style.display!='none')
+    {
+        showhidesem4.style.display='none';
+    }
+    else{
+        showhidesem4.style.display='block';
+    }
+    }
+
+    function sem5()
+    {
+    let showhidesem5=document.getElementById('showhidesem5');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem5.style.display!='none')
+    {
+        showhidesem5.style.display='none';
+    }
+    else{
+        showhidesem5.style.display='block';
+    }
+    }
+
+    function sem6()
+    {
+    let showhidesem6=document.getElementById('showhidesem6');
+    let btnshowhide=document.getElementById('btnshowhide');
+    if(showhidesem6.style.display!='none')
+    {
+        showhidesem6.style.display='none';
+    }
+    else{
+        showhidesem6.style.display='block';
+    }
+    }
